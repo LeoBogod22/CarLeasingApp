20cff4b977885eaea06aac6f49a542db67eb5346
arenuzzz <combatstriker111@gmail.com>
2020-03-09 16:38:41 +0200

    fix server error js

--- server.js
+++ server.js
@@ -1,67 +1,65 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+var express = require("express");
+var path = require("path");
+var favicon = require("serve-favicon");
+var logger = require("morgan");
+var cookieParser = require("cookie-parser");
+var bodyParser = require("body-parser");
 
-var index = require('./server/routes/routes');
-
+var index = require("./server/routes/routes");
 
 var app = express();
 
-app.use(function (req, res, next) {
+app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
   next();
 });
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// // view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-
-
-
-app.use('/', index);
+app.use("/", index);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+// app.use(function(req, res, next) {
+//   var err = new Error("Not Found");
+//   err.status = 404;
+//   next(err);
+// });
 
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
- 
 });
 
-if (process.env.NODE_ENV === 'production') {
+// if (process.env.NODE_ENV === "production") {
   // Serve any static files
-  app.use(express.static(path.join(__dirname, 'client/build')));
+  app.use(express.static(path.join(__dirname, "client/build")));
   // Handle React routing, return all requests to React app
-  app.get('*', function(req, res) {
-      res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "client/build", "index.html"));
   });
-}
+// }
 
 app.listen(4000, () => {
-  console.log("Ready")
-})
+  console.log("Ready");
+});
 
-module.exports = app;
+// module.exports = app;
--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,54 +1,63 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var nodemailer = require('nodemailer');
-const creds = require('../mailer/config/index');
+var nodemailer = require("nodemailer");
+const creds = require("../mailer/config/index");
 
 var transport = {
-  host: 'smtp.gmail.com',
-    port: 465,
-    secure: true,
+  host: "smtp.gmail.com",
+  // port: 465,
+  // secure: true,
   auth: {
     user: creds.USER,
     pass: creds.PASS
   }
-}
+};
 
-var transporter = nodemailer.createTransport(transport)
+// var transporter = nodemailer.createTransport(transport);
 
-transporter.verify((error, success) => {
-  if (error) {
-    console.log(error);
-  } else {
-    console.log('Server is ready to take messages');
-  }
-});
-router.post('/api/send', (req, res, next) => {
-  console.log("Request received", req.body)
+// transporter.verify((error, success) => {
+//   if (error) {
+//     console.log(error);
+//   } else {
+//     console.log("Server is ready to take messages");
+//   }
+// });
+router.post("/api/send", (req, res, next) => {
+  console.log("Request received", req.body);
   var name = req.body.name;
-  var email = req.body.email; 
-   var message = req.body.message;
+  var email = req.body.email;
+  var message = req.body.message;
   var content = `name: ${name} \n email: ${email} \n message: ${message} `;
 
   var mail = {
     from: name,
-    to: 'combatstriker111@gmail.com',  //Change to email address that you want to receive messages on
-    subject: 'New Message from Contact Form',
+    to: "combatstriker111@gmail.com", //Change to email address that you want to receive messages on
+    subject: "New Message from Contact Form",
     text: content
-  }
-    console.log(name);
-    console.log(email);
-    console.log(content);
-  transporter.sendMail(mail, (err, data) => {
-    if (err) {
-      res.json({
-        msg: 'fail '+ err.message
-      })
-    } else {
-      res.json({
-        msg: 'success'
-      })
-    }
-  })
-})
+  };
+  console.log(name);
+  console.log(email);
+  console.log(content);
+  // transporter.sendMail(mail, (err, data) => {
+  //   if (err) {
+  //     res.json({
+  //       msg: "fail " + err.message
+  //     });
+  //   } else {
+  //     res.json({
+  //       msg: "success"
+  //     });
+  //   }
+  // });
+});
+
+// if (process.env.NODE_ENV === "production") {
+//   // Serve any static files
+//   app.use(express.static(path.join(__dirname, "client/build")));
+//   // Handle React routing, return all requests to React app
+//   app.get("*", function(req, res) {
+//     res.sendFile(path.join(__dirname, "client/build", "index.html"));
+//   });
+// }
 
 module.exports = router;

45620e9dbab80730b8b074c57d98c9559b29f0f7
arenuzzz <combatstriker111@gmail.com>
2020-03-09 16:48:25 +0200

    edit server.js

--- server.js
+++ server.js
@@ -39,15 +39,15 @@ app.use("/", index);
 //   next(err);
 // });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+// // error handler
+// app.use(function(err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get("env") === "development" ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-});
+//   // render the error page
+//   res.status(err.status || 500);
+// });
 
 // if (process.env.NODE_ENV === "production") {
   // Serve any static files

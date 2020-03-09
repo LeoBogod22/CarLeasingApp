640cc4dc81d65d356ef0a3f5979bc8a37346fa7f
arenuzzz <combatstriker111@gmail.com>
2020-03-09 14:48:57 +0200

    fix server

--- server.js
+++ server.js
@@ -28,8 +28,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
 
+
+
+app.use('/', index);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -48,6 +50,15 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
  
 });
+
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'client/build')));
+  // Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+      res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
 
 app.listen(4000, () => {
   console.log("Ready")
--- server/routes/routes.js
+++ server/routes/routes.js
@@ -51,13 +51,13 @@ router.post('/api/send', (req, res, next) => {
   })
 })
 
-if (process.env.NODE_ENV === 'production') {
-    // Serve any static files
-    app.use(express.static(path.join(__dirname, 'client/build')));
-    // Handle React routing, return all requests to React app
-    app.get('*', function(req, res) {
-        res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-    });
-}
+// if (process.env.NODE_ENV === 'production') {
+//     // Serve any static files
+//     app.use(express.static(path.join(__dirname, 'client/build')));
+//     // Handle React routing, return all requests to React app
+//     app.get('*', function(req, res) {
+//         res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+//     });
+// }
 
 module.exports = router;
